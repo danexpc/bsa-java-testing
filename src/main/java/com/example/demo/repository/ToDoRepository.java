f9cc28d1540a8b7f80f614dc6dbd3de247b7e3e3
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-07-05 13:55:10 +0300

    Extend ToDoRepository interface from JpaSpecificationExecutor

--- src/main/java/com/example/demo/repository/ToDoRepository.java
+++ src/main/java/com/example/demo/repository/ToDoRepository.java
@@ -3,7 +3,8 @@ package com.example.demo.repository;
 import com.example.demo.model.ToDoEntity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ToDoRepository extends JpaRepository<ToDoEntity, Long> {
+public interface ToDoRepository extends JpaRepository<ToDoEntity, Long>, JpaSpecificationExecutor<ToDoEntity> {
 
 }
