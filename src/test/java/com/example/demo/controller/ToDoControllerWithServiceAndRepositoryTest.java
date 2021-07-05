504e294cc061cc1e1bd4a0233078c4f513d80dbf
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-07-05 20:44:58 +0300

    Add annotation which is reset autoincrement before each test

--- src/test/java/com/example/demo/controller/ToDoControllerWithServiceAndRepositoryTest.java
+++ src/test/java/com/example/demo/controller/ToDoControllerWithServiceAndRepositoryTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -25,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(classes = DemoApplicationTestConfig.class)
 @AutoConfigureMockMvc
 @ActiveProfiles(profiles = "test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class ToDoControllerWithServiceAndRepositoryTest {
 
     @Autowired
